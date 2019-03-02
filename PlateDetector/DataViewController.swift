47c9beb2eedfd50ccf30a5666196e3be2cfb1564
Wesley <wesleyswanson97@gmail.com>
2019-03-02 12:23:43 -0800

    Initialization.

--- PlateDetector/DataViewController.swift
+++ PlateDetector/DataViewController.swift
@@ -7,23 +7,75 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
+    let locationManager = CLLocationManager()
 
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        self.startReceivingLocationChanges()
+        var latitude = CLLocationDegrees(-33.86);
+        var longitude = CLLocationDegrees(151.20);
+        if let latitudeCoord = locationManager.location?.coordinate.latitude {
+            latitude = latitudeCoord
+        }
+        if let longitudeCoord = locationManager.location?.coordinate.longitude {
+            longitude = longitudeCoord
+        }
+        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        marker.title = "Your Location"
+//        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.dataLabel!.text = dataObject
+        self.dataLabel?.text = dataObject
+    }
+    
+    func startReceivingLocationChanges() {
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
+            // User has not authorized access to location information.
+            return
+        }
+        // Do not start services that aren't available.
+        if !CLLocationManager.locationServicesEnabled() {
+            // Location services is not available.
+            return
+        }
+        // Configure and start the service.
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0  // In meters.
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
 
+    func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last!
+        
+        // Do something with the location.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        if let error = error as? CLError, error.code == .denied {
+            // Location updates are not authorized.
+            manager.stopUpdatingLocation()
+            return
+        }
+        // Notify the user of any errors.
+    }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+target 'PlateDetector' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PlateDetector
+
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+
+  target 'PlateDetectorTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PlateDetectorUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
